883b24d2d16d11d622048304f1efd30e3da546f0
Yevhenii199 <gshijanov5@gmail.com>
2026-05-02 14:43:39 +0200

    feat: enable vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics"; // Добавь этот импорт
 import App from "./App.tsx";
 import "./i18n";
 import "./index.css";
+
+// Активация аналитики Vercel
+inject(); 
 
 createRoot(document.getElementById("root")!).render(<App />);
